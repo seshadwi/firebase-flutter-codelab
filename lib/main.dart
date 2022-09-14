1d4c61d0d8c8f785de2a85671f555ab844bd5ea7
seshadwi <seshaadwi@gamil.com>
2022-09-14 15:01:18 +0700

    firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Firebase Meetup',
       theme: ThemeData(
         buttonTheme: Theme.of(context).buttonTheme.copyWith(
